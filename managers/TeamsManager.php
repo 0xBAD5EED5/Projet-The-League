fa8966a40a33e53cb62f9b9bea02ee915e3af647
0xBAD5EED5 <eyestech404@protonmail.com>
2025-07-03 13:29:25 +0000

    Ajout de la classe TeamsManager avec les méthodes findAll et findOne pour gérer les équipes

---
+++ managers/TeamsManager.php
@@ -0,0 +1,49 @@
+<?php
+
+class TeamsManager extends AbstractManager {
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function findAll(): array {
+        $stmt = $this->db->query("SELECT * FROM teams");
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $teams = [];
+
+        foreach ($rows as $row) {
+            $mediaManager = new MediaManager();
+            $logo = $mediaManager->findOne($row['alt']);
+
+            $team = new Teams(
+                $row['name'],
+                $row['description'],
+                $logo
+            );
+            $team->setId($row['id']);
+            $teams[] = $team;
+        }
+        return $teams;
+    }
+
+    public function findOne(int $id): ?Teams {
+        $stmt = $this->db->prepare("SELECT * FROM teams WHERE id = :id");
+        $stmt->execute([':id' => $id]);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($row) {
+            $mediaManager = new MediaManager();
+            $logo = $mediaManager->findOne($row['alt']);
+
+            $team = new Teams(
+                $row['name'],
+                $row['description'],
+                $logo
+            );
+            $team->setId($row['id']);
+            return $team;
+        }
+        return null;
+    }
+}
