b5d0d159c228f1612442f5071b630978d6f0cc59
MLD-pro <morgan.ld.pro@gmail.com>
2025-07-03 08:55:19 +0000

    Push Media.php

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment($loader, [
+    'debug' => true,
+]);
+
+// Tableau associatif avec les infos des teams
+$teams = [
+    [
+        'name' => 'angry owls',
+        'img' => 'angry-owls.png',
+        'desc' => 'A team of angry owls',
+        'alt'  => "Logo de l'équipe Angry Owls, tête de hibou violet stylisé sur fond violet",
+        'link' => '#',
+    ],
+    [
+        'name' => 'chatty parrots',
+        'img' => 'chatty-parrots.png',
+        'desc' => 'A team of chatty parrots',
+        'alt'  => "Logo de l'équipe Chatty Parrots, perroquet stylisé avec des couleurs rouges et bleues",
+        'link' => '#',
+    ],
+    [
+        'name' => 'panthers',
+        'img' => 'panthers.png',
+        'desc' => 'A team of panthers',
+        'alt'  => "Logo de l'équipe Panthers, tête de panthère noire et bleue stylisée",
+        'link' => '#',
+    ],
+    [
+        'name' => 'sparrow',
+        'img' => 'sparrow.png',
+        'desc' => 'A team of sparrow',
+        'alt'  => "Logo de l'équipe Sparrow, oiseau bleu stylisé de profil",
+        'link' => '#',
+    ],
+    [
+        'name' => 'vendetta',
+        'img' => 'vendetta.png',
+        'desc' => 'A team of vendetta',
+        'alt'  => "Logo de l'équipe Vendetta, lettre V stylisée entourée d’un cercle lumineux bleu",
+        'link' => '#',
+    ],
+];
+
+// Passe le tableau à Twig ici :
+echo $twig->render("teams.html.twig", [
+    'teams' => $teams,
+]);
+?>
---
+++ models/Media.php
@@ -0,0 +1,48 @@
+<?php
+
+class Post {
+    private ?int $id = null;
+    private string $url;
+    private string $alt;
+
+    
+    public function __construct(string $url, string $alt) {
+        $this->title = $url;
+        $this->excerpt = $alt;
+    }
+
+
+
+    public function getId(): ?int 
+    {
+        return $this -> id;
+    }
+
+    public function getUrl(): string 
+    {
+        return $this -> url;
+    }
+    
+    public function getAlt(): string 
+    {
+        return $this -> alt;
+    }
+    
+
+    
+    
+    public function setId(int $id): void 
+    {
+        $this->id = $id;
+    }
+    
+     public function setUrl(string $url): void 
+    {
+        $this->url = $url;
+    }
+
+    public function setAlt(string $alt): void 
+    {
+        $this->alt = $alt;
+    }
+}
