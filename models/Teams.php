adea5a371fcb517a90d2e2bedb27909eef7717a2
0xBAD5EED5 <eyestech404@protonmail.com>
2025-07-03 08:48:41 +0000

    Ajout de la classe User avec les propriétés et méthodes pour gérer les informations des utilisateurs

---
+++ models/Teams.php
@@ -0,0 +1,42 @@
+<?php
+
+class User
+{
+    private ?int $id = null;
+
+    public function __construct(
+        private string $name,
+        private string $description,
+        private string $logo
+    ) {}
+
+    // ID
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    // Name
+    public function getName(): string
+    {
+        return $this->name;
+    }
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    // Logo
+    public function getLogo(): string
+    {
+        return $this->logo;
+    }
+    public function setLogo(string $logo): void
+    {
+        $this->logo = $logo;
+    }
+}
